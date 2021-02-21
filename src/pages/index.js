712a5a948bf8a04a5346a2f924554beddeca2fa2
kapil jhajhria <jhajhria44@gmail.com>
2021-02-21 16:12:47 +0530

    emails ection completed

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import SEO from "../components/seo"
 import Stats from "../components/Stats"
 import Testimonials from "../components/Testimonials"
 import { Trips } from "../components/Trips"
+import Email from "../components/Email"
 
 const IndexPage = () => (
   <Layout>
@@ -14,6 +15,7 @@ const IndexPage = () => (
     <Trips heading="Our Favorite Destinations" />
     <Testimonials />
     <Stats />
+    <Email />
   </Layout>
 )
 
